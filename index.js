e4945b99ed25a3cbf7c44426f48efdff5d420016
Mohit Chauhan <mo0001chauhan@gmail.com>
2024-01-28 16:52:39 +0530

    Tick-Tac-Toe Version 1.0.0

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const boxes = document.querySelectorAll(".box");
-let turn = 1;
+const res = document.querySelector(".result");
 let valid = true;
+let turn = 1;
 let valid_p1 = true;
 let valid_p2 = false;
+let win = false;
+//feature hover over elements
 for (const box of boxes) {
   box.addEventListener("mouseover", (event) => {
     if (turn == 1 && valid == true && valid_p1 == true && valid_p2 == false) {
@@ -23,18 +26,26 @@ for (const box of boxes) {
     }
   });
 }
+//logic of Choosing move
 function choose(Id) {
   let clss;
   let block = document.getElementById(Id);
-  if (turn == 1 && valid_p2 == false && valid_p1 == true) {
+  if (turn == 1 && valid_p2 == false && valid_p1 == true && valid == true) {
     clss = "dot";
+    block.innerText = "O";
     turn = 2;
     valid_p1 = false;
     valid_p2 = true;
     block.classList.add(clss);
     block.classList.remove("box", "cross");
-  } else if (turn == 2 && valid_p1 == false && valid_p2 == true) {
+  } else if (
+    turn == 2 &&
+    valid_p1 == false &&
+    valid_p2 == true &&
+    valid == true
+  ) {
     clss = "cross";
+    block.innerText = "X";
     turn = 1;
     valid_p1 = true;
     valid_p2 = false;
@@ -42,14 +53,45 @@ function choose(Id) {
     block.classList.remove("box", "dot");
   }
   block.setAttribute("disabled", true);
-}
-function reset() {
-  for (const box of boxes) {
-    box.classList.remove("cross");
-    box.classList.remove("dot");
-    box.classList.add("box");
+  if (turn == 1 && valid == true) {
+    res.innerText = "O's Turn";
+  } else if (turn == 2 && valid == true) {
+    res.innerText = "X's Turn";
   }
-  // document.querySelectorAll(".b").setAttribute("disabled", false);
-  document.querySelectorAll(".b").forEach(button => button.disabled = false);
 }
+// Logic of the game
 
+const win_pattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+for (const box of boxes) {
+  box.addEventListener("click", () => {
+    const check_winner = () => {
+      for (let pattern of win_pattern) {
+        let p1_val = boxes[pattern[0]].innerText;
+        let p2_val = boxes[pattern[1]].innerText;
+        let p3_val = boxes[pattern[2]].innerText;
+        if (
+          p1_val != "" &&
+          p2_val != "" &&
+          p3_val != "" &&
+          p1_val == p2_val &&
+          p2_val == p3_val &&
+          valid == true
+        ) {
+          res.innerText = `${p1_val} is winner`;
+          valid = false;
+          win = true;
+        }
+      }
+    };
+    check_winner();
+  });
+}
