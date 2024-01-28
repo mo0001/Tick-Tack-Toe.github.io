aad1f8d4c8c0fa05eb0475fd80b08f57f3108cb7
Mohit Chauhan <mo0001chauhan@gmail.com>
2024-01-28 14:07:21 +0530

    New feature

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
 const boxes = document.querySelectorAll(".box");
 let turn = 1;
 let valid = true;
-let hover = true;
-if (hover == true) {
-  for (const box of boxes) {
-    box.addEventListener("mouseover", (event) => {
-      if (turn == 1 && valid==true) {
-        event.target.classList.add("dot_hv");
-        box.innerText="O"
-      } else if (turn == 2 && valid == true) {
-        event.target.classList.add("cross_hv");
-        box.innerText="X"
-      }
-    });
-    box.addEventListener("mouseleave", (event) => {
-      if (valid == true) {
-        event.target.classList.remove("dot_hv");
-        event.target.classList.remove("cross_hv");
-        box.innerText = "";
-      }
-    });
+let valid_p1 = true;
+let valid_p2 = false;
+for (const box of boxes) {
+  box.addEventListener("mouseover", (event) => {
+    if (turn == 1 && valid == true && valid_p1 == true && valid_p2 == false) {
+      event.target.classList.add("dot_hv");
+    } else if (
+      turn == 2 &&
+      valid == true &&
+      valid_p2 == true &&
+      valid_p1 == false
+    ) {
+      event.target.classList.add("cross_hv");
+    }
+  });
+  box.addEventListener("mouseleave", (event) => {
+    if (valid == true) {
+      event.target.classList.remove("dot_hv");
+      event.target.classList.remove("cross_hv");
+    }
+  });
+}
+function choose(Id) {
+  let clss;
+  let block = document.getElementById(Id);
+  if (turn == 1) {
+    clss = "dot";
+    turn = 2;
+    valid_p1 = false;
+    valid_p2 = true;
+  } else if (turn == 2) {
+    clss = "cross";
+    turn = 1;
+    valid_p1 = true;
+    valid_p2 = false;
   }
+  block.classList.add(clss);
+  block.classList.remove("box");
+  block.ariaDisabled = true;
 }
-function choose(Id){
-let clss;
-if(turn==1){
-  clss="dot"
-} 
-else if(turn==2){
-clss="cross"
-}
-let block = document.getElementById(Id);
-block.classList.add(clss);
-block.classList.remove("box");
-}
-function reset(){
+function reset() {
   for (const box of boxes) {
     box.classList.remove("cross");
     box.classList.remove("dot");
     box.classList.add("box");
   }
 }
-
-
-
